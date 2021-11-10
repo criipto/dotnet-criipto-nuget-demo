4d068e51ae3d5a3054bb449eaa0840fbf29d7bd7
Rune Lund-Søltoft <rune@Runes-MacBook-Pro.local>
2021-11-10 13:12:35 +0100

    Renaming web to src and adding .coonfig so that tools can be restore

---
+++ src/Controllers/HomeController.fs
@@ -0,0 +1,32 @@
+﻿namespace Criipto.NugetSample.Controllers
+
+open System.Diagnostics
+open Microsoft.AspNetCore.Authorization
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+open Criipto.NugetSample.Models
+
+type HomeController (logger : ILogger<HomeController>) =
+    inherit Controller()
+
+    [<Authorize>]
+    member this.Index () =
+        this.View()
+
+    member this.Privacy () =
+        this.View()
+
+    member this.Callback() =
+        RedirectToActionResult("Index", "ControllerName",obj())
+
+    [<ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)>]
+    member this.Error () =
+        let reqId = 
+            if isNull Activity.Current then
+                this.HttpContext.TraceIdentifier
+            else
+                Activity.Current.Id
+
+        this.View({ RequestId = reqId })
