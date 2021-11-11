355c0c52934b0fdc8b98cbd1419f56b24c5a6443
Rune Lund-Søltoft <rune.lund.soltoft@criipto.com>
2021-11-11 13:05:18 +0100

    Only using user secrets when in development

--- src/Program.fs
+++ src/Program.fs
@@ -13,11 +13,13 @@ module Program =
                 webBuilder.UseStartup<Startup>() |> ignore
             ).ConfigureAppConfiguration(fun hostingContext configBuilder ->
                 let env = hostingContext.HostingEnvironment.EnvironmentName
-                configBuilder
-                    .AddJsonFile("appsettings.json")
-                    .AddJsonFile(sprintf "appsettings.%s.json" env, optional = true, reloadOnChange = true)
-                    .AddUserSecrets<Startup>(true, reloadOnChange = true)
-                    .AddEnvironmentVariables()
+                let builder = 
+                    configBuilder
+                        .AddJsonFile("appsettings.json")
+                        .AddJsonFile(sprintf "appsettings.%s.json" env, optional = true, reloadOnChange = true)
+                        .AddEnvironmentVariables()
+                if hostingContext.HostingEnvironment.IsDevelopment() then
+                    builder.AddUserSecrets<Startup>(true, reloadOnChange = true)
                     |> ignore
             )
 
